52c3ec535c7dc67982a5fc0f1283013b55c49631
NOHP404 <52324396+NOHP404@users.noreply.github.com>
2023-02-27 14:43:29 +0700

    add light gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   darkMode: "class",
   content: ["./*.html","./js/*.js"],
   theme: {
+    fontFamily: {
+      "sweed-display": ["Montserrat", "sans-serif"],
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
       "sweed-gray": "#151515",
+      "sweed-gray-light" : "#444444",
       "sweed-dark": "#000000",
       "sweed-color": "#A7D3AA",
+      "sweed-color-light": "#A7D3C0",
       "sweed-white": "#EAEAEA",
     },
     extend: {},
