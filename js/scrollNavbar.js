f2104c5a3bea3a49f5c1693fc367b758a6328e59
NOHP404 <52324396+NOHP404@users.noreply.github.com>
2023-02-28 20:24:34 +0700

    transparent navbar

--- js/scrollNavbar.js
+++ js/scrollNavbar.js
@@ -2,16 +2,22 @@
 window.onscroll = function() {scrollFunction()};
 
 // Get the navbar
-var navbar = document.querySelector("nav");
+var navbar = document.querySelector("#mainMenuNavbar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
+navbar.classList.add("bg-transparent")
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollFunction() {
   if (window.scrollY > sticky) {
+    navbar.classList.remove("absolute")
+    navbar.classList.remove("bg-transparent")
     navbar.classList.add("fixed")
+    navbar.classList.add("border-b-2")
   } else {
-    navbar.classList.remove("fixed");
+    navbar.classList.add("absolute")
+    navbar.classList.add("bg-transparent")
+    navbar.classList.remove("fixed")
+    navbar.classList.remove("border-b-2")
   }
 }
