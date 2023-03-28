532eabed41fffdeedfc37578144dd42fd6d4aacd
NOHP404 <52324396+NOHP404@users.noreply.github.com>
2023-03-28 21:23:43 +0700

    making ejs 0.1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
 
 app.use(express.static("public"));
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
