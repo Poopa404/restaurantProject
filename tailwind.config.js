ee48b6e684f821a625eb8efe23b01b310088d644
NOHP404 <52324396+NOHP404@users.noreply.github.com>
2023-03-28 17:13:02 +0700

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./html/*.html",
-            "./js/*.js",
+  content: ["./public/*/*.{html,js,css}",
             "./views/*.ejs"],
   theme: {
     fontFamily: {
