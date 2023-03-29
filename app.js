e37cc2517df40235e63a782234e581b452aed5a3
NOHP404 <52324396+NOHP404@users.noreply.github.com>
2023-03-29 16:19:59 +0700

    add link to navbar, login and register

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ app.listen("3000", () => {
 app.get("/", (req, res) => {
   res.render("homePage");
 })
+
+app.get("/menu", (req, res) => {
+  res.render("menu");
+})
+
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+app.post("/cart", (req, res) => {
+  res.render("shoppingCart");
+})
+
+app.all("/*", (req, res) => {
+  res.redirect("/")
+})
